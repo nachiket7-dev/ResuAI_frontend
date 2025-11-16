20b497a92a19aa20829a958d27c492cbeafeb154
Nachiket Amlekar <nachiketamlekar@Nachikets-MacBook-Pro.local>
2025-11-16 21:14:53 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import pdfToText from 'react-pdftotext'
 
 const Dashboard = () => {
 
-    const {token} = useSelector((state) => state.auth);
+    const {token, user} = useSelector((state) => state.auth);
 
 
     const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"]
@@ -118,7 +118,7 @@ const Dashboard = () => {
   return (
     <div>
         <div className='max-w-7xl mx-auto px-4 py-8'>
-            <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, John Doe</p>
+            <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, {user?.name || 'User'}</p>
             <div className='flex gap-4'>
                 <button onClick={() => setShowCreateResume(true)} className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all'>
                     <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full' />
